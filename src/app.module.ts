58d1f7e30ed7f14c42151e781e82f9bd91e38672
Gabriel Pampuch <gabripampuch@hotmail.com>
2023-08-01 16:46:09 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoursesModule } from './modules/courses/courses.module';
+import { UsersModule } from './modules/users/users.module';
+
+@Module({
+  imports: [CoursesModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
